056b7056e40a97c32a95b60216320ceed6ddecee
Roman Chaban <roman.chaban1001@gmail.com>
2024-06-07 23:43:21 +0300

    Fixed Hero Slider.

--- src/components/HeroSlider/HeroSlider.tsx
+++ src/components/HeroSlider/HeroSlider.tsx
@@ -28,22 +28,29 @@ export const HeroSlider: FC = () => {
             <div className={styles.heroSlider__information}>
               <div className={styles.heroSlider__logo}>
                 <Apple color='white' style={{ width: 40, height: 40 }} />
-                <h3 className={styles.heroSlider__title}>iPhone 14 Series</h3>
+                <h3 className={styles.heroSlider__title}>
+                  Apple MacBook Pro 14 M3
+                </h3>
               </div>
               <h2 className={styles.heroSlider__discount}>
-                Up to 10% off Voucher
+                Up to 15% off Voucher
               </h2>
               <Link className={styles.heroSlider__link} href='/Shop'>
                 Shop now! <FormNextLink color='white' />
               </Link>
             </div>
-            <div className={styles.heroSlider__block}>
+            <div
+              className={[
+                styles.heroSlider__block,
+                styles.heroSlider__first,
+              ].join(' ')}
+            >
               <Image
                 priority
                 className={styles.heroSlider__image}
-                src='/images/hero/hero.svg'
+                src='/images/hero/macbook-pro.jpg'
                 alt='hero image'
-                width={496}
+                width={396}
                 height={344}
               />
             </div>
